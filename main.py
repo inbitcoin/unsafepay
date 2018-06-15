d553fb417a25f378e3214036e777102cdbe8329c
Martino Salvetti <tldr@inbitcoin.it>
2018-06-15 10:03:48 +0200

    Link to chan page on 1ml.com instead of chan_in

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ ALLOWED_COMMANDS = {
 }
 _24H = 60 * 60 * 24
 TX_LINK = 'https://www.smartbit.com.au/tx/%s'
-
+CH_LINK = 'https://1ml.com/channel/%s'
 
 def to_btc_str(sats):
     return '{:.8f}'.format(Decimal(sats) / Decimal(1e8))
@@ -123,7 +123,7 @@ class Lncli:
             pubkey = ch['remote_pubkey']
             active = '\u26a1\ufe0f' if ch['active'] else '\U0001f64a'
             rows.append('%s %s' % (self.aliases.get(pubkey, pubkey), active))
-            rows.append(ch['chan_id'])
+            rows.append(CH_LINK % ch['chan_id'])
             rows.append(to_btc_str(ch['capacity']))
             local = to_btc_str(ch['local_balance'])
             remote = to_btc_str(ch['remote_balance'])
