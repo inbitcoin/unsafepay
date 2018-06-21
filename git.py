928dfc5b5f6d779570259859a64b26184b0055d3
Martino Salvetti <tldr@inbitcoin.it>
2018-06-21 18:46:32 +0200

    Formatting

--- git.py
+++ git.py
@@ -5,6 +5,8 @@ import subprocess
 def get_git_revision_short_hash():
     try:
         with open(os.devnull, 'w') as devnull:
-            return str(subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], stderr=devnull), 'ascii').strip()
+            return str(subprocess.check_output(
+                ['git', 'rev-parse', '--short', 'HEAD'],
+                stderr=devnull), 'ascii').strip()
     except subprocess.CalledProcessError:
         pass
