6b15ab472369229b17ca84e1f8f1dd31213a88b3
Martino Salvetti <tldr@inbitcoin.it>
2018-06-21 17:01:46 +0200

    User git rev-parse HEAD to detect the last commit

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-LAST=$(git log | head -n 1)
+LAST=$(git rev-parse HEAD)
 git pull
-CURR=$(git log | head -n 1)
+CURR=$(git rev-parse HEAD)
 if [ "$LAST" != "$CURR" ]
 then
     tmux kill-session -t unsafepay
